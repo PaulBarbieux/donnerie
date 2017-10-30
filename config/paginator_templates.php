09d5eef860e73c81d6ddafd5a7636db956e9f463
Paul <paul.barbieux@gmail.com>
2017-10-30 20:44:09 +0100

    Liens en boutons.

---
+++ config/paginator_templates.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'nextActive' => '<A href="{{url}}" class="btn btn-primary">{{text}}</A>',
+	'nextDisabled' => '<A class="btn btn-primary disabled">{{text}}</A>',
+	'prevActive' => '<A href="{{url}}" class="btn btn-primary">{{text}}</A>',
+	'prevDisabled' => '<A class="btn btn-primary disabled">{{text}}</A>',
+	'first' => '<A href="{{url}}" class="btn btn-primary">{{text}}</A>',
+	'last' => '<A href="{{url}}" class="btn btn-primary">{{text}}</A>'
+];
+?>
