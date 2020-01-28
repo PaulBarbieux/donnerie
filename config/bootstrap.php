3e52d1adc87ab9092df433e775c16a57d50a3308
PaulBarbieux <paul.barbieux@gmail.com>
2020-01-28 20:47:23 +0100

    Suppression des "%" dans les noms d'image.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -212,7 +212,7 @@ if (Configure::read('debug')) {
 
 // Transform file name
 function goodFileName($fileName) {
-	$fileName = str_replace(" ","_",$fileName);
+	$fileName = str_replace(array(" ","%","&"),"_",$fileName);
 	$search = explode(",","ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,ø,Ø,Å,Á,À,Â,Ä,È,É,Ê,Ë,Í,Î,Ï,Ì,Ò,Ó,Ô,Ö,Ú,Ù,Û,Ü,Ÿ,Ç,Æ,Œ");
 	$replace = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,o,O,A,A,A,A,A,E,E,E,E,I,I,I,I,O,O,O,O,U,U,U,U,Y,C,AE,OE");
 	return str_replace($search, $replace, $fileName);
